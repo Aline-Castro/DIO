d89074a28ff51436ba08d255d870d00d0e61a1fe
Aline Castro <92234598+Aline-Castro@users.noreply.github.com>
2022-09-29 11:31:53 -0300

    Update alfabeto.py

--- Geração Tech Unimed-BH - Ciência de Dados/Desafios intermediários em Python/alfabeto.py
+++ Geração Tech Unimed-BH - Ciência de Dados/Desafios intermediários em Python/alfabeto.py
@@ -28,3 +28,15 @@ alfabeto = {"A":1, "B":2, "C":3, "D":4, "E":5, "F":6, "G":7, "H":8, "I":9, "J":1
             "O":15, "P":16, "Q":17, "R":18, "S":19, "T":20, "U":21, "V":22, "W":23, "X":24, "Y":25, "Z":26}
 print(alfabeto[index])
 
+
+
+# entrada = input("") 
+# letra= entrada.upper()
+
+# imprima a posição dessa letra no Alfabeto;
+# lista = [chr(i) for i in range(ord('A')-1,ord('Z')+1)]
+# print (lista.index(letra))
+
+#
+#
+
